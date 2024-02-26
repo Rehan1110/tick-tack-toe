05ce8c964a49933ad1d5dbc25f829ff625894d5d
Rehan Abbas <abbasrehan054@gmail.com>
2024-02-27 00:30:06 +0530

    Fix the box from undefined to the marker

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
-const gameboard = (() => {
+const Gameboard = (() => {
   let gameboard = [ "", "", "", "", "", "", "", "", ""];
 
   const render = () =>{
     let boardHTML = "";
     gameboard.forEach((square, index) => {
-     boardHTML += `<div class="square" id=square-${index}>${index}</div>`
-    })
-  }
+     boardHTML += `<div class="square" id=square-${index}>${square}</div>`
+    });
     document.querySelector("#gameboard").innerHTML = boardHTML;
+    const squares = document.querySelectorAll('.square');
+ squares.forEach((square) => {
+   square.addEventListener("click", Game.handleClick);
+ })
+  }
+
+  const update = (index, value) => {
+    gameboard[index] = value;
+    render();
+  }
+   
     return {
       render,
+      update
     }
 })();
 
@@ -17,19 +28,28 @@ const createPlayer = (name, marker) =>{
 return {name, marker};
 }
 
-const Game = () =>{
+const Game = (() =>{
   let players = [];
   let currentPlayerIndex;
-  let gameOver = false;
+  let gameOver;
 
   const start = () =>{
  players = [
-  createPlayer(document.querySelector("#player1").value, "X"),
-  createPlayer(document.querySelector("player2").value, "0")
- ]
+  createPlayer(document.querySelector("#player-1").value, "X"),
+  createPlayer(document.querySelector("#player-2").value, "0")
+ ];
+ currentPlayerIndex = 0;
+ gameOver = false;
+ Gameboard.render();
   }
-}
+  function handleClick(event) {
+    let index = parseInt(event.target.id.split("-")[1]);
+    Gameboard.update(index, players[currentPlayerIndex].marker);
+  }
+  return {start,handleClick};
+})();
 
 let startButton = document.querySelector("#start-button");
 startButton.addEventListener("click",()=>{
+  Game.start();
 })
