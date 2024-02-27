f669a1480e3a06b1b2c7a85ede456d34cdfe18e4
Rehan Abbas <abbasrehan054@gmail.com>
2024-02-27 16:10:59 +0530

    Added the display function for the winner

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+const displayController = (() => {
+  const renderMessage = (message) => {
+    document.querySelector("#message").innerHTML = message;
+  }
+  return {
+    renderMessage,
+  }
+})()
+
 const Gameboard = (() => {
   let gameboard = [ "", "", "", "", "", "", "", "", ""];
 
@@ -49,22 +58,58 @@ const Game = (() =>{
 })
   }
   const handleClick = (event) => {
+    if(gameOver) return;
     let index = parseInt(event.target.id.split("-")[1]);
     if (Gameboard.getGameboard()[index] !== "")
     return 
     Gameboard.update(index, players[currentPlayerIndex].marker);
+    
+    if(checkForWin(Gameboard.getGameboard(), players[currentPlayerIndex].marker)) {
+      gameOver = true;
+      displayController.renderMessage(`${players[currentPlayerIndex].name} wins`)
+    } else if (checkForTie(Gameboard.getGameboard())) {
+      gameOver = true;
+      displayController.renderMessage("It's a tie");
+    }
+
     currentPlayerIndex = currentPlayerIndex === 0 ? 1 : 0;
   }
 
   const restart = () => {
     for ( let i = 0; i < 9; i++) {
-       Game.update(i, "");
+       Gameboard.update(i, "");
     }
-    Game.render();
+    Gameboard.render();
+    gameOver = false;
+    document.querySelector("#message").innerHTML ="";
   }
 
-  return {start,handleClick};
+  return {start, handleClick, restart};
 })();
+
+function checkForWin(board) {
+  const winningCombinations =[
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  for ( let i = 0; i < winningCombinations.length; i++) {
+    const [a, b, c] =winningCombinations[i];
+    if(board[a] && board[b] && board[a] === board[c]) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function checkForTie(board) {
+ return board.every(cell => cell !== "");
+}
 
 const restartButton = document.querySelector("#restart-button");
 restartButton.addEventListener("click", () => {
